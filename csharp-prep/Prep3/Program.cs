ab1bef5d0bd8279a6dcc0fdc4f03b730f9848ee0
lrtay123 <lrtay123@yahoo.com>
2024-10-30 15:57:23 -0800

    Finish up guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+        Console.WriteLine(magicNumber);
+        
+        int number = 0; /// declare here.
+
+        do
+        {
+            
+            Console.Write("What is the magic number? ");
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+
+            if (number == magicNumber)
+            {
+                Console.WriteLine("Congrats! You got it write");
+            }
+            else if (number > magicNumber)
+            {
+                Console.WriteLine("Gues Lower");
+            }
+                        else if (number < magicNumber)
+            {
+                Console.WriteLine("Gues Higher");
+            }
+            
+        }while (number != magicNumber);
+
+        
     }
 }
