ebd6bc0b27cc9c00691d6ad9267a3f36b26a623c
lrtay123 <lrtay123@yahoo.com>
2024-10-30 12:12:38 -0800

    I created the grading tool

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your percentage in the class? ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+
+
+        if (percentage >= 90)
+        {
+            Console.WriteLine("You can an A!! ");
+            if (percentage > 70)
+            {
+                Console.WriteLine("You passed the class! ");
+            }
+        }
+        if (percentage < 90 && percentage >= 80)
+        {
+            Console.WriteLine("You can a B!! ");
+            if (percentage > 70)
+            {
+                Console.WriteLine("You passed the class! ");
+            }
+        }
+        if (percentage < 80 && percentage >= 70)
+        {
+            Console.WriteLine("You got a C!! ");
+            if (percentage > 70)
+            {
+                Console.WriteLine("You passed the class! ");
+            }
+        }
+        if (percentage < 70 && percentage >= 60)
+        {
+            Console.WriteLine("You got a D... ");
+            if (percentage > 70)
+            {
+                Console.WriteLine("You passed the class! ");
+            }
+            else
+            {
+                Console.WriteLine("You did not pass the Class");
+            }
+        }
+
+        if (percentage < 60)
+        {
+            Console.WriteLine("You got a F...");
+            if (percentage > 70)
+            {
+                Console.WriteLine("You passed the class! ");
+            }
+            else
+            {
+                Console.WriteLine("You did not pass the Class");
+            }
+
+        }
+
     }
 }
